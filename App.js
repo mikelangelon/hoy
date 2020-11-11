c0103cec2d8babc569db76552a0b353d0f5984a4
Miguel Corvo <mikelangelon@gmail.com>
2020-11-11 18:21:58 +0100

    adding textinput

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    if (newTask !== "") {
+      console.log("adding task " + newTask);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.appTitle}>Tasker</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder={"Add a new task"}
+        onChangeText={(text) => setNewTask(text)}
+        onSubmitEditing={addTask}
+        returnKeyType={"done"}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +30,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  appTitle: {
+    fontSize: 36,
+    fontWeight: "250",
+  },
+  input: {
+    padding: 20,
+    borderBottomWidth: 2,
+    fontSize: 18,
   },
 });
